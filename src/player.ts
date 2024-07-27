f185d2324b265f5024dded7dcab1f88cd00c99db
lsioctl <lsioctl@gmail.com>
2024-07-27 16:56:39 -0400

    moving with keyboard

--- src/player.ts
+++ src/player.ts
@@ -1,10 +1,10 @@
-import { Actor, vec } from "excalibur";
+import { Actor, vec, Keys } from "excalibur";
 import { Resources } from "./resources";
 
 export class Player extends Actor {
   constructor() {
     super({
-      pos: vec(150, 150),
+      pos: vec(500, 150),
       width: 100,
       height: 100
     });
@@ -15,5 +15,34 @@ export class Player extends Actor {
     this.on('pointerup', () => {
       alert('yo');
     });
+  }
+
+  public update(engine: ex.Engine, delta: number) {
+    super.update(engine, delta);
+    if (
+      engine.input.keyboard.isHeld(Keys.W) ||
+      engine.input.keyboard.isHeld(Keys.Up)
+    ) {
+      this.pos.y -= 5;
+    }
+    if (
+      engine.input.keyboard.isHeld(Keys.S) ||
+      engine.input.keyboard.isHeld(Keys.Down)
+    ) {
+      this.pos.y += 5;
+    }
+    if (
+      engine.input.keyboard.isHeld(Keys.D) ||
+      engine.input.keyboard.isHeld(Keys.Right)
+    ) {
+      this.pos.x += 5;
+    }
+    if (
+      engine.input.keyboard.isHeld(Keys.A) ||
+      engine.input.keyboard.isHeld(Keys.Left)
+    ) {
+      this.pos.x -= 5;
+    }
+    //console.log("update");
   }
 }
